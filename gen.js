28178437bb29d479b599bf55ab0814735321dce2
Nate McMaster <nate.mcmaster@microsoft.com>
2016-05-04 10:42:47 -0700

    Generate tfms from the GH issue

--- gen.js
+++ gen.js
@@ -2,7 +2,10 @@
 var fs = require('fs'),
 	path = require('path'),
 	mkdirp = require('mkdirp'),
-	extend = require('extend');
+	extend = require('extend'),
+	rimraf = require('rimraf');
+
+rimraf.sync('artifacts/');
 
 var base = {
 	"dependencies": {
@@ -13,18 +16,29 @@ var base = {
 	}
 };
 
-var tfms = ["netstandard1.1", "netstandard1.2", "netstandard1.3"];
+var frameworks = [
+	["net451"],
+	["net46"],
+	["net462"],
+	["netcoreapp1.0"],
+	["netstandard1.5"],
+	["netcore50"],
+	["net451", "net462", "netcoreapp1.0", "netstandard1.5"]
+];
 
-for (let tfm of tfms) {
+for (let f of frameworks) {
 	// copy the base obj
 	var project = {};
-	
+
 	// extend(deepCopy, target, source)
 	extend(true, project, base);
 
-    project.frameworks[tfm] = {};
+	for (let tfm of f) {
+		project.frameworks[tfm] = {};
+	}
+	var folderName = f.reduce((all,sum) => all +"." + sum);
 
-	var dir = path.join("artifacts/mvc",tfm);
+	var dir = path.join("artifacts/di", folderName);
 	mkdirp.sync(dir);
 
 	fs.writeFileSync(path.join(dir, "project.json"), JSON.stringify(project, null, 2));
