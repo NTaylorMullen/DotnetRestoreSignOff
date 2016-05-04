643ff61224da9d9e8f5769c3eb8c883ef268d3d9
Nate McMaster <nate.mcmaster@microsoft.com>
2016-05-04 11:16:57 -0700

    Add imports for mvc

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Generating project.json files"
 node gen.js
+
 dotnet="$(pwd)/.dotnet/dotnet"
 
 version="latest"
@@ -9,4 +11,7 @@ if [ ! -e $dotnet ]; then
 	curl https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash -s -- -i "$(pwd)/.dotnet" --version $version
 fi
 
+echo "Running dotnet restore --verbosity error"
 $dotnet restore --verbosity error artifacts/
+
+echo "Done."
--- gen.js
+++ gen.js
@@ -18,14 +18,16 @@ var base = {
 
 var packageSets = [
 	{
-		"Microsoft.Extensions.DependencyInjection": "1.0.0-*"
+		deps: { "Microsoft.Extensions.DependencyInjection": "1.0.0-*" },
+		imports: []
 	},
 	{
-		"Microsoft.AspNetCore.Mvc": "1.0.0-*"
+		deps: { "Microsoft.AspNetCore.Mvc": "1.0.0-*" },
+		imports: ["portable-dnxcore50+net45+win8+wp8+wpa81"]
 	}
 ];
 
-var frameworks = [
+var frameworkSets = [
 	["net451"],
 	["net46"],
 	["net462"],
@@ -39,32 +41,45 @@ var ignore = {
 	"Microsoft.AspNetCore.Mvc": ["netcore50"]
 };
 
-for (let f of frameworks) {
+for (let tfms of frameworkSets) {
 	// copy the base obj
 	var projectBase = {};
 
-	// extend(deepCopy, target, source)
+	// clone object syntax: extend(bool: deepCopy, obj: target, obj: source)
 	extend(true, projectBase, base);
 
-	for (let tfm of f) {
-		projectBase.frameworks[tfm] = {};
+	for (let f of tfms) {
+		projectBase.frameworks[f] = {};
 	}
-	var folderName = f.reduce((all, sum) => all + "." + sum);
 
-	for (let pkgs of packageSets) {
-		var firstPackageName = Object.keys(pkgs)[0];
+	// js version of string.Join('.', tfms)
+	var folderName = tfms.reduce((all, sum) => all + "." + sum);
+
+	for (let pkgSet of packageSets) {
+		var firstPackageName = Object.keys(pkgSet.deps)[0];
 
 		if (ignore[firstPackageName] && ignore[firstPackageName].indexOf(folderName) >= 0) {
 			continue;
 		}
 
+		// make aritfact dir
 		var dir = path.join("artifacts", firstPackageName, folderName);
 		mkdirp.sync(dir);
 
+		// this is what will be actually written to project.json
 		var project = {};
 		extend(true, project, projectBase);
-		for (let pkg in pkgs) {
-			project.dependencies[pkg] = pkgs[pkg];
+
+		for (let pkg in pkgSet.deps) {
+			// add dependencies at top level
+			project.dependencies[pkg] = pkgSet.deps[pkg];
+		}
+
+		if (pkgSet.imports) {
+			// add imports to all tfms 
+			for(let x in project.frameworks) {
+				project.frameworks[x].imports = pkgSet.imports;
+			}
 		}
 
 		fs.writeFileSync(path.join(dir, "project.json"), JSON.stringify(project, null, 2));
