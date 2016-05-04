e932093d7c4f03b95c468ca9bd8e3dd606771095
Nate McMaster <nate.mcmaster@microsoft.com>
2016-05-04 10:56:46 -0700

    Gen project.json for matrix of tfms and packages

--- gen.js
+++ gen.js
@@ -16,6 +16,12 @@ var base = {
 	}
 };
 
+var packageSets = [{
+	"Microsoft.Extensions.DependencyInjection": "1.0.0-*"
+}, {
+	"Microsoft.AspNetCore.Mvc": "1.0.0-*"
+}, ];
+
 var frameworks = [
 	["net451"],
 	["net46"],
@@ -28,18 +34,27 @@ var frameworks = [
 
 for (let f of frameworks) {
 	// copy the base obj
-	var project = {};
+	var projectBase = {};
 
 	// extend(deepCopy, target, source)
-	extend(true, project, base);
+	extend(true, projectBase, base);
 
 	for (let tfm of f) {
-		project.frameworks[tfm] = {};
+		projectBase.frameworks[tfm] = {};
 	}
-	var folderName = f.reduce((all,sum) => all +"." + sum);
+	var folderName = f.reduce((all, sum) => all + "." + sum);
 
-	var dir = path.join("artifacts/di", folderName);
-	mkdirp.sync(dir);
+	for (let pkgs of packageSets) {
+		var firstPackageName = Object.keys(pkgs)[0];
+		var dir = path.join("artifacts", firstPackageName, folderName);
+		mkdirp.sync(dir);
 
-	fs.writeFileSync(path.join(dir, "project.json"), JSON.stringify(project, null, 2));
+		var project = {};
+		extend(true, project, projectBase);
+		for (let pkg in pkgs) {
+			project.dependencies[pkg] = pkgs[pkg];
+		}
+
+		fs.writeFileSync(path.join(dir, "project.json"), JSON.stringify(project, null, 2));
+	}
 }
